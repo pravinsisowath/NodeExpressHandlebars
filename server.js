d5536b3edf0e008db9c79828e7212a8c3e4d9b67
Pravin Sisowath <pravinsisowath@gmail.com>
2020-07-07 01:13:23 -0700

    set up server and middleware

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const app = require('express')
+const express = require('express')
+const { join } = require('path')
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.engine('handlebars', require('express-handlebars'))
+app.set('view engine', 'handlebars')
+
+app.use(require('./routes'))
+
+require('dotenv').config()
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(PORT)
+)
